b39acf8f7fc3b5a0ef81b1009ae64e4cee12187b
Mark Jenkinson <mark@fmrib.ox.ac.uk>
2000-04-14 14:57:39 +0000

    Changed read_volume so that data is not read

--- rmsdiff.cc
+++ rmsdiff.cc
@@ -21,8 +21,10 @@
 int main(int argc,char *argv[])
 {
 
-  if (argc!=3) { 
-    cerr << "Usage: " << argv[0] << " matrixfile1 matrixfile2\n"; 
+  float rmax=80.0;
+
+  if ((argc<3) || (argc>4)) { 
+    cerr << "Usage: " << argv[0] << " matrixfile1 matrixfile2 [refvol]\n"; 
     return -1; 
   }
   
@@ -30,16 +32,25 @@ int main(int argc,char *argv[])
   if (read_ascii_matrix(affmat1,argv[1])<0)   return -2;
   if (read_ascii_matrix(affmat2,argv[2])<0)   return -2;
 
-  float rms = rms_deviation(affmat1,affmat2);
+  ColumnVector centre(3);
+  centre = 0;
+
+  if (argc==4) {
+    volume refvol;
+    short dtype;
+    read_volume(refvol,argv[3],dtype,false);
+    centre(1) = (refvol.xsize() - 1.0)/2.0;
+    centre(2) = (refvol.ysize() - 1.0)/2.0;
+    centre(3) = (refvol.zsize() - 1.0)/2.0;
+  }
+
+  float rms = rms_deviation(affmat1,affmat2,centre,rmax);
 
   cout << rms << endl;
   
   return 0;
 
 }
-
-
-
 
 
 
