2e5d417679fbf4c88489181a1c6133b846374d29
Duncan Mortimer <duncan.mortimer@ndcn.ox.ac.uk>
2007-07-31 16:00:25 +0000

    moving standard

--- flirt.tcl
+++ flirt.tcl
@@ -52,7 +52,7 @@ pack $w.f.nstats -in $lfstats -side top -anchor w -pady 3 -padx 5
 #}}}
     #{{{ standard image
 
-set entries($w,1) ${FSLDIR}/etc/standard/avg152T1_brain.hdr
+set entries($w,1) ${FSLDIR}/data/standard/avg152T1_brain.hdr
 
 FileEntry  $w.f.ref -textvariable entries($w,1) -label "Reference image   " -title "Select" -width 50 -filedialog directory  -filetypes IMAGE
 
