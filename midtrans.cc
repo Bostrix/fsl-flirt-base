bcbffc91bfcaad7fbfe219d43ed8c23594a66b81
Matthew Webster <mwebster@fmrib.ox.ac.uk>
2014-07-11 11:23:11 +0000

    really fixed this time

--- midtrans.cc
+++ midtrans.cc
@@ -361,7 +361,7 @@ int main(int argc,char *argv[])
     options.add(verbose);
     options.add(help);
     
-    nonoptarg = options.parse_command_line(argc, argv, true);
+    nonoptarg = options.parse_command_line(argc, argv, 0, true);
 
     // line below stops the program if the help was requested or 
     //  a compulsory option was not set
