85685ee784707aefb28e61ec3d08ae6213ac010b
Taylor Hanayik <taylor.hanayik@ndcn.ox.ac.uk>
2020-01-30 14:46:55 +0000

    TH changes for more interp choices, updated options/usage and modified 3D option arg.

--- applyxfm4D.cc
+++ applyxfm4D.cc
@@ -13,10 +13,9 @@ using namespace NEWIMAGE;
 
 // Globals - needed by fmrib_main
 
-string oname, iname, transname, refname, matprefix="/MAT_0";
-bool singlematrix, fourd;
-
-
+string oname, iname, transname, refname, usrinterp="sinc", matprefix="/MAT_0";
+bool singlematrix, fourd, verbose;
+interpolation interpmethod = sinc;
 //////////////////////////////////////////////////////////////////////
 
 template <class T>
@@ -30,12 +29,17 @@ int fmrib_main(int argc, char* argv[])
     for (int t=0; t<invol.tsize(); t++) {
       invol[t].setpadvalue(invol[t].backgroundval());
     }
+    if (verbose) {
+      cout << "using interpolation method (enum, string): " << interpmethod << ", " << usrinterp << endl;
+    }
     invol.setextrapolationmethod(extraslice);
-    invol.setinterpolationmethod(sinc);
-    invol.definesincinterpolation("b",7);
+    invol.setinterpolationmethod(interpmethod);
+    if (interpmethod == sinc) {
+        invol.definesincinterpolation("b",7);
+    }
 
     // old form used a volume number
-    //    refvol = invol[atoi(refname.c_str())];  
+    //    refvol = invol[atoi(refname.c_str())];
 
     read_volume(refvol,refname);
 
@@ -63,7 +67,7 @@ int fmrib_main(int argc, char* argv[])
 	cout << matname << endl;
 	affmat = read_ascii_matrix(matname);
       }
-      
+
       dummy = refvol;
       affine_transform(invol[m],dummy,affmat);
       outvol.addvolume(dummy);
@@ -74,16 +78,18 @@ int fmrib_main(int argc, char* argv[])
   } else {
     // 3D mode
     volume<T> invol, outvol;
-    
+
     read_volume(invol,iname);
     read_volume(outvol,refname);
     invol.setextrapolationmethod(extraslice);
-    invol.setinterpolationmethod(sinc);
-    invol.definesincinterpolation("r",9);
+    invol.setinterpolationmethod(interpmethod);
+    if (interpmethod == sinc) {
+        invol.definesincinterpolation("r",9);
+    }
 
     Matrix affmat(4,4);
     affmat = read_ascii_matrix(transname);
-    
+
     affine_transform(invol,outvol,affmat);
     outvol.setDisplayMaximumMinimum(0,0);
     save_volume(outvol,oname);
@@ -92,45 +98,80 @@ int fmrib_main(int argc, char* argv[])
   return 0;
 }
 
+interpolation get_interptype(string usrinterp) {
+    if (usrinterp.compare("nearestneighbour") == 0 || usrinterp.compare("nn") == 0) {
+      return nearestneighbour;
+    }
+    else if (usrinterp.compare("trilinear") == 0) {
+      return trilinear;
+    }
+    else if (usrinterp.compare("sinc") == 0) {
+      return sinc;
+    }
+    else if (usrinterp.compare("spline") == 0) {
+      return spline;
+    }
+    else {
+      // if no matches
+      cerr << "interpolation method: " << usrinterp << " is not a valid choice. Please choose from nearestneighbour, trilinear, sinc, or spline." << endl;
+      exit(EXIT_FAILURE);
+    }
+}
+
 
 int main(int argc,char *argv[])
 {
 
   Tracer tr("main");
-  if (argc<5) { 
+  if (argc<5) {
     cerr << "Usage: " << argv[0] << " <input volume> <ref volume>"
-	 << " <output volume> <transformation matrix file/[dir]> [-singlematrix/-fourdigit/-userprefix <prefix>]]\n"; 
-    return -1; 
+    << " <output volume> <transformation matrix file/dir>\n\n" <<
+    "\t--interp, -interp <nearestneighbour (or nn), trilinear, spline, sinc (default)>\n" <<
+    "\t--singlematrix, -singlematrix (flag option, do not provide an argument)\n" <<
+    "\t--fourdigit, -fourdigit (flag option, do not provide an argument)\n" <<
+    "\t--userprefix, -userprefix <prefix>\n" << endl;
+    return -1;
   }
-  
+
   // NB: a hidden option (-3D) exists (must appear after singlematrix)
-
-
   // parse the command line
-  oname = argv[3];
-  iname = argv[1];
-  transname = argv[4]; 
-  refname = argv[2];
   singlematrix = false;
-  if (argc>=6) {
-    string option = argv[5];
-    if (option == "-singlematrix" )  singlematrix = true;
-    if (option == "-fourdigit" )  matprefix = "/MAT_";
-    if ( (option == "-userprefix" ) && (argc>=7) ) {
-      string uprefix = argv[6];
-      matprefix = "/" + uprefix;
-    }
-  }
   fourd = true;
-  if (argc>=7) {
-    string option = argv[6];
-    if (option == "-3D" )  fourd = false;
+  verbose = false;
+  // first four arguments are postional and must be in the correct order
+  iname = argv[1];
+  refname = argv[2];
+  oname = argv[3];
+  transname = argv[4];
+  for (int i = 5; i < argc; ++i){
+    string option = argv[i];
+    if (option == "-singlematrix" || option == "--singlematrix") {
+        singlematrix = true;
+    }
+    else if (option == "-fourdigit" || option == "--fourdigit") {
+        matprefix = "/MAT_";
+    }
+    else if (option == "-userprefix" || option == "--userprefix") {
+        if (i++ <= argc) {
+            string uprefix = argv[i];
+            matprefix = "/" + uprefix;
+        }
+    }
+    else if (option == "-interp" || option == "--interp") {
+        if (i++ <= argc) {
+            usrinterp = argv[i];
+            interpmethod = get_interptype(usrinterp);
+        }
+    }
+    else if (option == "-3D" || option == "--3D" || option == "-3d" || option == "--3d") {
+        fourd = false;
+    }
+    else if (option == "--verbose" || option == "-verbose" || option == "-v") {
+        verbose = true;
+    }
   }
 
   // call the templated main
   return call_fmrib_main(dtype(iname),argc,argv);
 
 }
-
-
-
